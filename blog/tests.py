2a00c760eb61f4659d75c3663acd7be422bdc240
Y9372 <yndbsk9372@gmail.com>
2022-10-18 15:31:08 +0900

    포스트 상세페이지 테스트

--- blog/tests.py
+++ blog/tests.py
@@ -8,7 +8,6 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
-
     def test_post_list(self):
         response = self.client.get('/blog/')
         # response 결과가 정상적으로 보이는지
@@ -43,3 +42,23 @@ class TestView(TestCase):
         self.assertIn(post_001.title, main_area.text)
         self.assertIn(post_002.title, main_area.text)
         self.assertNotIn("아직 게시물이 없습니다", main_area.text)
+    def test_post_detail(self):
+        post_001 = Post.objects.create(title="첫번째 포스트", content="첫번째 포스트입니다")
+        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+
+        response = self.client.get(post_001.get_absolute_url(), follow=True)
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # navbar가 정상적으로 보이는지
+        navbar = soup.nav
+        self.assertIn('BLOG', soup.nav.text)
+        self.assertIn('ABOUT ME', navbar.text)
+
+        self.assertIn(post_001.title, soup.title.text)  #앞서서 post-area을 보겠다는 것이고 이것은 전체적인 title에 포함이 되어져있는가를 확인하는 것
+
+        main_area = soup.find('div', id='main-area')
+        post_area = main_area.find('div', id='post-area')
+        self.assertIn(post_001.title, post_area.text)
+        self.assertIn(post_001.content, post_area.text)
+
