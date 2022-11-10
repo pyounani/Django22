97943f2024eab9da177f0c1a63073fcca06d9507
Y9372 <yndbsk9372@gmail.com>
2022-11-10 12:16:30 +0900

    포스트 수정 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
+    path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('create_post/', views.PostCreate.as_view()),
     path('category/<str:slug>/', views.category_page),  #IP주소/blog/category/slug/
     path('tag/<str:slug>/', views.tag_page),
