322ca90928fa713e0a3c897e3ae926471f7aad9c
Y9372 <yndbsk9372@gmail.com>
2022-11-17 12:25:01 +0900

    회원가입과 로그인 기능 추가하기

--- myDjangoPrj/urls.py
+++ myDjangoPrj/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path('', include('single_pages.urls'))
+    path('', include('single_pages.urls')),
+    path('accounts/', include('allauth.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
